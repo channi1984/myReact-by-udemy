f6b35428aa622cb4ec91471d3773e04c29b5f74d
channi84 <channi.kr@gmail.com>
2021-03-26 17:30:02 +0900

    'bb

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './component/search_bar';
+
 const API_KEY = 'AIzaSyB46ndi7CLQOj0zB1wSFtGU9u8ISPmHhzk';
 
 const App = () => {
-    return <div>Hi</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
