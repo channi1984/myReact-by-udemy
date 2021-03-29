ca19c8ee012e0b0999c3b5702f13bade6d09f042
channi84 <channi.kr@gmail.com>
2021-03-29 16:15:11 +0900

    '비디오-리스트-작성'

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './component/search_bar';
-
+import VideoList from './component/video_list';
 const API_KEY = 'AIzaSyC2lyhSWJHdoDd1rOGmgiM8WmY0gXbF8Pc';
-//API가 안먹힐대는 API 할달량이 제한걸린건데, 프로젝트 다시 생성해서 API 새로 발급 해야함
+//API가 안먹힐때는 API 할달량이 제한걸린건데, 프로젝트 다시 생성해서 API 새로 발급 해야함
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function (date) {
-    console.log(date)
-})
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+        this.state = { videos: [] }
+
+        YTSearch({ key: API_KEY, term: 'keyboard' }, (videos) => {
+            this.setState({ videos })
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        )
+    }
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
